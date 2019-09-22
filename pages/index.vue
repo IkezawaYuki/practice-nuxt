e651462f5be5f9b1064f9bebd983fd480648cb06
yikezawa <y.ikezawa93@gmail.com>
2019-09-22 23:38:24 +0900

    modified file

---
+++ pages/index.vue
@@ -0,0 +1,48 @@
+<template>
+  <section class="container">
+    <div>
+      <h3>Nuxt.jsのタグがつけられた投稿一覧</h3>
+      <ul>
+        <li v-for="item in items" :key="item.id">
+
+        </li>
+      </ul>
+    </div>
+  </section>
+</template>
+
+<script>
+import AppLogo from '~/components/AppLogo.vue'
+
+export default {
+  async asyncData({app}){
+    const items = await app.$axios.$get('https://qiita.com/api/v2/items?query=tag:nuxt.js')
+    return {
+      items
+    }
+  }
+}
+</script>
+
+<style>
+.container {
+ min-height: 100vh;
+ padding: 16px;
+}
+
+h3{
+  margin: 16px 0;
+  padding: 8px 0;
+  border-bottom: solid 1px #e5e5e5;
+}
+
+li + li{
+  margin: 16px 0;
+}
+
+p{
+  margin: 8px 0;
+}
+
+</style>
+

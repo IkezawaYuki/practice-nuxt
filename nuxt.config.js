a7828f2da3a0e994154ecd73f2848d67352e6f21
yikezawa <y.ikezawa93@gmail.com>
2019-09-23 16:12:01 +0900

    modified file

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,6 +4,7 @@ module.exports = {
   */
   head: {
     title: 'my-first-nuxt-app',
+    titleTemplate: '%s | Nuxt.js tag items viewer',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -22,6 +23,11 @@ module.exports = {
   ],
   axios:{
 
+  },
+  router:{
+    middleware:[
+      'auth'
+    ]
   },
   plugins:[
     '~/plugins/axios.js'
--- pages/index.vue
+++ pages/index.vue
@@ -1,35 +1,29 @@
 <template>
-  <section class="container">
-    <div>
-      <h3>Nuxt.jsのタグがつけられた投稿一覧</h3>
-      <ul>
-        <li v-for="item in items" :key="item.id">
-          <h4>
-            <span>{{item.title}}</span>
-            <small>
-              <span> by </span>
-              <nuxt-link :to="`/users/${item.user.id}`">
-                {{ item.user.id }}
-              </nuxt-link>
-            </small>
-          </h4>
-          <div>{{ item.body.slice(0, 130)}}・・・</div>
-          <p><a :href="item.url">{{ item.url }}</a></p>
-        </li>
-      </ul>
-    </div>
-  </section>
+  <div>
+    <h2>index page</h2>
+    <ul>
+      <li>
+        <nuxt-link to="/login">ログインページへ</nuxt-link>
+      </li>
+      <li>
+        <nuxt-link to="/authed-route">認証が必要なページへ</nuxt-link>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
+import {mapGetters} from 'vuex'
 
 export default {
-  async asyncData({app}){
-    const items = await app.$axios.$get('https://qiita.com/api/v2/items?query=tag:nuxt.js')
-    return {
-      items
+  async asyncData({store}){
+    if(store.getters['items'].length){
+      return
     }
+    await store.dispatch('fetchItems')
+  },
+  computed:{
+    ...mapGetters(['items'])
   }
 }
 </script>
